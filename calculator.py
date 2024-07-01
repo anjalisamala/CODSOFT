c540929f4761ff3021d177f1f3c73d00dc31dab2
anjalisamala <anjalisamala03@gmail.com>
2024-07-02 00:08:02 +0530

    variables initialized

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def calculate():
+    operation=input('''
+please type the mathematical operation you would like to complete:
+                    + for addition
+                    - for subtraction
+                    * for multiplication
+                    / for division
+                    ''')
+    
+    num1=int(input("Enter First Number : "))
+    num2=int(input("Enter Second Number : "))
