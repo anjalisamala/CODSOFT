5d2b58ef24855083dc8913810be2232786941c7f
anjalisamala <anjalisamala03@gmail.com>
2024-07-01 23:55:34 +0530

    two functions added

---
+++ toDo_list.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter import messagebox
+import sqlite3 as sql
+
+def add_task():
+    task_string=task_field.get()
+    if len(task_string) == 0:  
+        messagebox.showinfo('Error', 'Field is Empty.')  
+    else:    
+        tasks.append(task_string)   
+        the_cursor.execute('insert into tasks values (?)', (task_string ,))    
+        list_update()    
+        task_field.delete(0, 'end')  
+    
+def list_update():    
+    clear_list()    
+    for task in tasks:    
+        task_listbox.insert('end', task)  
