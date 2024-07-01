fd0231d5f59a9eb55f0231cb64998ab72c0a47fc
anjalisamala <anjalisamala03@gmail.com>
2024-07-02 00:09:28 +0530

    conditions of if/else

--- calculator.py
+++ calculator.py
@@ -9,3 +9,25 @@ please type the mathematical operation you would like to complete:
     
     num1=int(input("Enter First Number : "))
     num2=int(input("Enter Second Number : "))
+
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+
+    elif operation == '/':
+        if number_2 != 0:
+            print('{} / {} = '.format(number_1, number_2))
+            print(number_1 / number_2)
+        else:
+            print("Error! Division by zero is not allowed.")
+
+    else:
+        print('You have not typed a valid operator, please run the program again.')
